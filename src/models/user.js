d85cd7e9b6480d1b6dea69464c9744405db0a40a
Aharon Lubitsch <59685123+koftov@users.noreply.github.com>
2020-12-15 19:47:05 +0200

    trim and lowercase to username and email t to pass

--- src/models/user.js
+++ src/models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
 
 const User = mongoose.model("User", {
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, trim: true, lowercase: true },
+  password: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true, lowercase: true },
   bio: { type: String, default: "Please enter some bio." },
   avatar: { type: String, default: "no-img.jpg" },
   followers: [ObjectId],
